263f8b331cf75b94870dc7e79b4a609b738b52b9
Daniell Algar <daniell.algar@gmail.com>
2018-08-15 10:15:37 +0200

    Initial commit

---
+++ app/src/main/java/com/algar/boendekalkyl/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.algar.boendekalkyl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.navigation_fragment).navigateUp()
+    }
+}
