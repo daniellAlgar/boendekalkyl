b868a90afaad8f39a88ac3ee2c43644b4ce8060f
Daniell Algar <daniell.algar@gmail.com>
2018-08-16 09:11:43 +0200

    Setup bottom navigation

---
+++ app/src/main/java/com/algar/boendekalkyl/CalculateFragment.kt
@@ -0,0 +1,18 @@
+package com.algar.boendekalkyl
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CalculateFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_calculate, container, false)
+    }
+}
--- app/src/main/java/com/algar/boendekalkyl/MainActivity.kt
+++ app/src/main/java/com/algar/boendekalkyl/MainActivity.kt
@@ -3,12 +3,22 @@ package com.algar.boendekalkyl
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.navigation_fragment)
+        setupActionBarWithNavController(this, navController)
+        setupWithNavController(bottom_navigation, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
